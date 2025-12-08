2638e75fd139ab6ec3056ad04fabf5e6dca96539
mostafa ibrahim <mostafaibrahim2942004@gmail.com>
2025-12-08 07:59:09 +0200

    valid email

--- Application/Features/Auth/Commands/VerifyOtp/VerifyOtpCommandValidator.cs
+++ Application/Features/Auth/Commands/VerifyOtp/VerifyOtpCommandValidator.cs
@@ -9,9 +9,6 @@ namespace Application.Features.Auth.Commands.VerifyOtp
     {
         public VerifyOtpCommandValidator()
         {
-            RuleFor(x => x.Email)
-                .NotEmpty().WithMessage("Email is required.")
-                .EmailAddress().WithMessage("A valid email is required.");
             RuleFor(x => x.OtpCode)
                 .NotEmpty().WithMessage("OTP code is required.")
                 .Length(6).WithMessage("OTP code must be 6 characters long.");
