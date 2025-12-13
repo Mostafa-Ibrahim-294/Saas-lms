a7161b47fc1d4c611ef0b3eaa0c1a99439e5b286
mostafa ibrahim <mostafaibrahim2942004@gmail.com>
2025-12-13 22:58:05 +0200

    ت

---
+++ Api/Controllers/TenantsController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Constants;
+using Application.Features.Tenants.Queries.GetLastTenant;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = AuthConstants.ApiScheme)]
+
+    public class TenantsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TenantsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        
+    }
+}
