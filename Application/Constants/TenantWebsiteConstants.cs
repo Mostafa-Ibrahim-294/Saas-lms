2bc34736e3eee1f93a9a9673207047148f13d871
ibrahem nasr <ibrahem nasr@gmail.com>
2026-02-28 21:47:16 +0200

    fix Bug For PageBlocksResponse again

--- Application/Constants/TenantWebsiteConstants.cs
+++ Application/Constants/TenantWebsiteConstants.cs
@@ -4,7 +4,7 @@
     {
         public const string HeroId = "hero";
         public const string TextId = "text";
-        public const string Featured_CoursesId = "featured_courses";
+        public const string Featured_CoursesId = "featured-courses";
         public const string TestimonialsId = "testimonials";
         public const string CtaId = "cta";
         public const string FooterId = "footer";
