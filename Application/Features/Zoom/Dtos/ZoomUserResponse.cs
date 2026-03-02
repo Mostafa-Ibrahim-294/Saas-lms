6a792160a2545e6e5eea227d2d40e1c1a5606569
ibrahem nasr <ibrahem nasr@gmail.com>
2026-03-02 21:00:18 +0200

    fix bug again

--- Application/Features/Zoom/Dtos/ZoomUserResponse.cs
+++ Application/Features/Zoom/Dtos/ZoomUserResponse.cs
@@ -7,7 +7,7 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Type { get; set; } =  string.Empty;
+        public int Type { get; set; } 
         public string Status { get; set; } = string.Empty;
 
     }
