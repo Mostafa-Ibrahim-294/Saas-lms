37f58f85b2c02421ca619feeab6008d5ffff58de
ibrahem nasr <ibrahem nasr@gmail.com>
2026-04-13 16:37:12 +0200

    Fix Bug For GetTenantStudent Endpoint

--- Application/Features/TenantStudents/Dtos/StudentDto.cs
+++ Application/Features/TenantStudents/Dtos/StudentDto.cs
@@ -8,7 +8,6 @@
         public string Grade { get; set; } = string.Empty;
         public string ProfilePicture { get; set; } = string.Empty;
         public int AverageGrades { get; set; }
-        public DateTime LastLogin { get; set; }
         public StudentFlagDto Flags { get; set; } = new();
         public List<int>? EnrolledCourses { get; set; } = new();
         public List<StudentCourseDto> Courses { get; set; } = new();
