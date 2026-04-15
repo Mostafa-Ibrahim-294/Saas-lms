e9d0df210135a4f727caa8af16f2a80ab0dced1e
ibrahem nasr <ibrahem nasr@gmail.com>
2026-04-15 14:27:44 +0200

    add configuration enrollment

---
+++ Infrastructure/Persistence/Configurations/EnrollmentConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public sealed class EnrollmentConfigurations : IEntityTypeConfiguration<Enrollment>
+    {
+        public void Configure(EntityTypeBuilder<Enrollment> builder)
+        {
+            builder.HasIndex(e => new { e.StudentId, e.CourseId })
+                   .IsUnique();
+
+            builder.HasOne(e => e.Student)
+                   .WithMany(s => s.Enrollments)
+                   .HasForeignKey(e => e.StudentId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(e => e.Course)
+                   .WithMany(c => c.Enrollments)
+                   .HasForeignKey(e => e.CourseId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(e => e.Tenant)
+                   .WithMany(t => t.Enrollments)
+                   .HasForeignKey(e => e.TenantId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
